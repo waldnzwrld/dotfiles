8ab083fc17646cc877f6d207fef64ce207286d3d
Walden Bodtker <waldnzwrld@github.com>
2021-11-04 10:04:57 -0700

    just do the debian

--- install.sh
+++ install.sh
@@ -2,10 +2,5 @@
 
 set -e
 
-cp .gitconfig ~/.gitconfig
-cp .zshrc ~/.zshrc
-
-export DEBIAN_FRONTEND=noninteractive
-
 sudo apt-get update
 sudo apt-get -y install zsh-autosuggestions zsh-syntax-highlighting jq ripgrep fzf
