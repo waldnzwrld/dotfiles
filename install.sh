7478d6f94dd5a22882af1e4702f607df364a45fc
Walden Bodtker <waldnzwrld@github.com>
2021-11-04 13:31:05 -0700

    drop gitconfig for now

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ sudo apt-get update
 sudo apt-get -y install zsh-autosuggestions zsh-syntax-highlighting jq ripgrep fzf
 
 cp .zshrc $HOME/.zshrc
-cp .gitconfig $HOME/.gitconfig
+# cp .gitconfig $HOME/.gitconfig
