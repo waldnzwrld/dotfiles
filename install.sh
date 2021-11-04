5379f30fc3781f86691e085ac0a120279dc42741
Walden Bodtker <waldnzwrld@github.com>
2021-11-04 10:22:04 -0700

    try with frontend

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
-set -e
+set -xe
+
+export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
 sudo apt-get -y install zsh-autosuggestions zsh-syntax-highlighting jq ripgrep fzf
